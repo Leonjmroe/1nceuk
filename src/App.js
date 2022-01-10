24dd98d92b3827375bd1e9867eea70682cb7a23b
leonjmroe <leonjmroe@gmail.com>
2022-01-10 16:18:57 +0000

    1nce under development

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+import logo from './images/1nce-logo.png';
+import underdevlogo from './images/under-dev-logo.png';
 import './App.css';
 
 function App() {
@@ -6,17 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={underdevlogo} className="under-dev-logo" alt="logo" />
+        <a className="under-dev-text" href="https://www.instagram.com/1nce.uk/"> 1nce under development </a>
       </header>
     </div>
   );
