3c34bc0b843d7840ad9e56ece321dcfec27c71e2
leonjmroe <leonjmroe@gmail.com>
2022-02-03 19:57:17 +0000

    home url

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           <Router>  
           <Navbar />
           <Routes>
-            <Route path="/home" element={<Home />} />
+            <Route path="/" element={<Home />} />
             <Route path="/store" element={<Store />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
