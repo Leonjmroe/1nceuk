37b4e07b5538ac2b8fd5b16d60043dfd30c6ca64
leonjmroe <leonjmroe@gmail.com>
2022-02-08 19:58:23 +0000

    footer content

--- src/components/core/footer.js
+++ src/components/core/footer.js
@@ -3,6 +3,19 @@ import './../core/core.css';
 export default function Footer() {
 
 return (
-  <div className="footer"/>
+  <div className="footer">
+  	<div className="box">
+	  <div className="icon icon1"/>
+	  <div className="iconTxt">Instagram</div>
+  	</div>
+  	<div className="box">
+  	  <div className="icon icon2"/>
+	  <div className="iconTxt">Depop</div>
+  	</div>
+  	<div className="box">
+  	  <div className="icon icon3"/>
+	  <div className="iconTxt">Email</div>
+  	</div>
+  </div>
 )}
 
