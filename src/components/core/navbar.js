bd80b94d5b17538f05d9d11d74d505974914c3d0
leonjmroe <leonjmroe@gmail.com>
2022-02-10 21:45:17 +0000

    mobile optimisation

--- src/components/core/navbar.js
+++ src/components/core/navbar.js
@@ -1,40 +1,39 @@
 import './../core/core.css';
 import { useNavigate } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 
 
 export default function Header() {
 
   const navigate = useNavigate();
 
-  const dropdown = () => {
+  const dropdown = dropdown => {
     console.log(100)
   }
+
+  const useStyles = makeStyles(() => ({
+      dropdown_box: {
+        position: 'absolute',
+        'display': 'none'
+      }
+    }))
+
+  const classes = useStyles();
 
 return (
 
 <div className="header">
   <div className="logo1nce" onClick={()=> navigate('/CSSDesign')}/>
   <div className="storeTitle">Store</div>
-  <div className="dropdown">
-    {/*<ul class="dropdown_menu" onClick={()=> dropdown()}>
-      <li><a href="#">Designs</a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Contact</a></li>
-    </ul>*/}
-  </div>
-  {/*<div class="dropdown">
-    <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example
-    <span class="caret"></span></button>
-    <ul class="dropdown-menu">
-      <li><a href="#">HTML</a></li>
-      <li><a href="#">CSS</a></li>
-      <li><a href="#">JavaScript</a></li>
-    </ul>
-  </div>*/}
+  <div className="dropdown" onClick={()=> dropdown()}/>
+  <ul className={classes.dropdown_box}>
+    <li><a href="#">Designs</a></li>
+    <li><a href="#">About</a></li>
+    <li><a href="#">Contact</a></li>
+  </ul>
   <div className="basketCont">  
     <div className="shopCounter">1</div>
     <div className="shopBtn" onClick={()=> navigate('/checkout')}/>
   </div>
 </div>
 )}
-
