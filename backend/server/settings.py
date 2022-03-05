ef5a17e9952d64c514ed9d47995b16f83e191e02
leonjmroe <leonjmroe@gmail.com>
2022-03-05 18:12:55 +0000

    server fix attempt

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -4,9 +4,11 @@ from pathlib import Path
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = 'django-insecure-=a*)s^uet7ai@!c6muqtt4w__+9enk@jx$+23g7iv!v!p#qja3'
+
+#SECRET_KEY = 'django-insecure-=a*)s^uet7ai@!c6muqtt4w__+9enk@jx$+23g7iv!v!p#qja3'
 DEBUG = False 
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['127.0.0', '1nceuk.com']
+ALLOWED_HOSTS = []
 
 
 INSTALLED_APPS = [
