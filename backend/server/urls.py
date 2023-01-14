789ee1d09943268be5ee3d6ef2a17f6d3d0aa19f
Leon <leon@Leons-MacBook-Pro.local>
2023-01-14 19:43:30 +0000

    url fix attempt 2

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from apps.accounts.urls import accounts_urlpatterns
 from django.conf.urls.static import static
@@ -9,8 +9,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html')),  
     path('api/items/', include('apps.items.urls')),
-    # path(r'^$', TemplateView.as_view(template_name='index.html')),  
-    # path(r'^(?:.*)/?$', TemplateView.as_view(template_name='index.html')),
+    re_path(r'^$', TemplateView.as_view(template_name='index.html')),  
+    re_path(r'^(?:.*)/?$', TemplateView.as_view(template_name='index.html')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += accounts_urlpatterns  
