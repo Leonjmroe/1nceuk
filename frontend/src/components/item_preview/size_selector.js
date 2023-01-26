7cbf90b896b194e080971e86b1871350a3b78267
Leon <leon@Leons-MacBook-Pro.local>
2023-01-26 10:55:11 +0000

    re-engineer size_selector componenet

--- frontend/src/components/item_preview/size_selector.js
+++ frontend/src/components/item_preview/size_selector.js
@@ -10,70 +10,65 @@ export default function SizeSelector(props) {
     const [size_medium, set_size_medium] = useState(css.size_element)
     const [size_large, set_size_large] = useState(css.size_element)
     const [size_extra_large, set_size_extra_large] = useState(css.size_element)
-    const [style_switch, set_style_switch] = useState(css.size_cont)
+    const [cont_style_switch, set_cont_style_switch] = useState(css.size_cont)
+    const [element_style_switch, set_element_style_switch] = useState(css.size_element)
 
     useEffect(() => {
-      stock_check()
       if( props.type == "computer" ) {
-        set_style_switch(css.size_cont)
+        set_cont_style_switch(css.size_cont)
+        set_element_style_switch(css.size_element)
       } else {
-        set_style_switch(css.size_cont_mobile)
+        set_cont_style_switch(css.size_cont_mobile)
+        set_element_style_switch(css.size_element_mobile)
       }
+      stock_check()
     }, []);
 
     const stock_check = () => {
       if(location.state.item.qty_small == 0) {
-        set_size_small(`${css.size_element} ${css.no_stock}`)
+        set_size_small(`${element_style_switch} ${css.no_stock}`)
       }else {
-        set_size_small(css.size_element)
+        set_size_small(element_style_switch)
       }
       if(location.state.item.qty_medium == 0) {
-        set_size_medium(`${css.size_element} ${css.no_stock}`)
+        set_size_medium(`${element_style_switch} ${css.no_stock}`)
       }else {
-        set_size_medium(css.size_element)
+        set_size_medium(element_style_switch)
       }
       if(location.state.item.qty_large == 0) {
-        set_size_large(`${css.size_element} ${css.no_stock}`)
+        set_size_large(`${element_style_switch} ${css.no_stock}`)
       }else {
-        set_size_large(css.size_element)
+        set_size_large(element_style_switch)
       }
       if(location.state.item.qty_extra_large == 0) {
-        set_size_extra_large(`${css.size_element} ${css.no_stock}`)
+        set_size_extra_large(`${element_style_switch} ${css.no_stock}`)
       }else {
-        set_size_extra_large(css.size_element)
+        set_size_extra_large(element_style_switch)
       }
     }
 
     const size_select = (size, qty) => {
       if(size == "S" && qty != 0) {
         stock_check()
-        set_size_small(`${css.size_element} ${css.size_select}`)
+        set_size_small(`${element_style_switch} ${css.size_select}`)
       }else if(size == "M" && qty != 0) {
         stock_check()
-        set_size_medium(`${css.size_element} ${css.size_select}`)
+        set_size_medium(`${element_style_switch} ${css.size_select}`)
       }else if(size == "L" && qty != 0) {
         stock_check()
-        set_size_large(`${css.size_element} ${css.size_select}`)
+        set_size_large(`${element_style_switch} ${css.size_select}`)
       }else if (size == "XL" && qty != 0){
         stock_check()
-        set_size_extra_large(`${css.size_element} ${css.size_select}`)
+        set_size_extra_large(`${element_style_switch} ${css.size_select}`)
       }
     } 
 
    return (
-    <div className={style_switch}>
-      <div className={css.size_element_cont}>
-        <div className={size_small} onClick={() => { size_select('S', location.state.item.qty_small) }}>S</div>
-      </div>   
-      <div className={css.size_element_cont}>                                            
-        <div className={size_medium} onClick={() => { size_select('M', location.state.item.qty_medium) }}>M</div>
-      </div>     
-      <div className={css.size_element_cont}>                                             
-        <div className={size_large} onClick={() => { size_select('L', location.state.item.qty_large) }}>L</div>
-      </div>    
-      <div className={css.size_element_cont}>                                               
-        <div className={size_extra_large} onClick={() => { size_select('XL', location.state.item.qty_extra_large) }}>XL</div>
-      </div>                                                  
+    <div className={cont_style_switch}>
+      <div className={size_small} onClick={() => { size_select('S', location.state.item.qty_small) }}>S</div>                                           
+      <div className={size_medium} onClick={() => { size_select('M', location.state.item.qty_medium) }}>M</div>                                             
+      <div className={size_large} onClick={() => { size_select('L', location.state.item.qty_large) }}>L</div>                                            
+      <div className={size_extra_large} onClick={() => { size_select('XL', location.state.item.qty_extra_large) }}>XL</div>                                                
     </div>
   );
 }
