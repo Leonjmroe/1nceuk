27991a5c3ab21eb6624b3909d5b5d6cacd671b7e
Leon <leon@Leons-MacBook-Pro.local>
2023-03-26 03:47:51 +0100

    checkout page UI update

--- frontend/src/components/checkout/checkout.js
+++ frontend/src/components/checkout/checkout.js
@@ -1,4 +1,5 @@
 import css from './checkout.module.css';
+import Countries from './countries.js';
 import { useContext, useEffect, useState } from "react";
 import { StateContext } from '../state_management/context.js'
 import ItemTile from './../store/itemTile.js';
@@ -10,6 +11,7 @@ export default function Preview() {
   const location = useLocation()
   const [state, dispatch] = useContext(StateContext)
   const [items, set_items] = useState([])
+  const [payment, set_payment] = useState()
 
   const getData = key => {
     return JSON.parse(window.localStorage.getItem(key));
@@ -20,9 +22,26 @@ export default function Preview() {
     if( item_data != null ) {
       dispatch({ type: 'set_basket', payload: item_data })
       set_items(item_data)
+      set_payment(location.state.payment)
     }
-    console.log(item_data, location.state)
   }, []);
+
+  
+  const addressAPI = event => { 
+
+    var requestOptions = {
+      method: 'GET',
+    };
+    fetch(`https://api.geoapify.com/v1/geocode/autocomplete?text=${event.target.value}&apiKey=bf1de4840f1e4e868aed2942d329321b`, requestOptions)
+      .then(response => response.json())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+  }
+
+  const summary_title  = () => {
+    const output = ('Total: ' + payment + ' (' + items.length + ' Items)')
+    return output
+  }
 
 
   return (
@@ -30,6 +49,9 @@ export default function Preview() {
       <div className={css.checkout_cont}>
         <div className={css.checkout_title}>Checkout</div>
         <div className={css.checkout_preview_cont}>
+          <div className={css.summary_cont}>  
+            <div className={css.summary_title}>{summary_title()}</div>
+            </div>
           <div className={css.details_cont}>
             <input className={css.first_name} type="text" placeholder="First Name"></input>
             <input className={css.last_name} type="text" placeholder="Last Name"></input>
@@ -37,13 +59,13 @@ export default function Preview() {
             <input className={css.phone_number} type="text" placeholder="Phone Number"></input>
           </div>
           <div className={css.address_cont}>
-            <input className={css.address_line_1} type="text" placeholder="Address Line 1"></input>
+            <input className={css.address_line_1} onChange={addressAPI} type="text" placeholder="Address Line 1"></input>
             <input className={css.address_line_2} type="text" placeholder="Address Line 2"></input>
             <input className={css.address_line_3} type="text" placeholder="Address Line 3"></input>
             <input className={css.address_city} type="text" placeholder="City/Locality"></input>
             <input className={css.address_area} type="text" placeholder="State/Province"></input>
             <input className={css.address_postcode} type="text" placeholder="Postal Code"></input>
-            <input className={css.address_country} type="dropdown" placeholder="Country"></input>
+            <Countries />
           </div>
           <div className={css.payment_button}>Payment</div>
         </div>
