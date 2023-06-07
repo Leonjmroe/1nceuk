6c0802e4b73dbe8985099482241e87a1bcec8b16
Leonjmroe <leonjmroe@gmail.com>
2023-06-07 13:13:28 +0100

    skate page iframe bug fix

--- frontend/src/components/skating/skating.js
+++ frontend/src/components/skating/skating.js
@@ -11,11 +11,11 @@ export default function Skating() {
               Within this community, it is customary to acknowledge skateboarders by admiring their attire. Our ultimate objective was to establish a company that not only caters to the clothing needs of skaters but also understands their subculture. By supplying the local skating community with a distinct identity, we aimed to facilitate recognition and connection among skaters of all levels. Through the medium of clothing, 1nceuk endeavors to unify us all, transcending individual skill levels.
           </div>
           <div className={css.text}>Text about skate vid 1</div>
-          <iframe className={css.video} src="https://www.youtube.com/watch?v=OoE-yuzmoHQ&t=5s"></iframe>
-          <iframe className={css.video} src="https://www.youtube.com/watch?v=OoE-yuzmoHQ&t=5s"></iframe>
+          <iframe className={css.video} width="560" height="315" src="https://www.youtube.com/embed/OoE-yuzmoHQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <iframe className={css.video} width="560" height="315" src="https://www.youtube.com/embed/OoE-yuzmoHQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
           <div className={css.text}>Text about skate vid 2</div>
           <div className={css.text}>Text about skate vid 3</div>
-          <iframe className={css.video} src="https://www.youtube.com/watch?v=OoE-yuzmoHQ&t=5s"></iframe>
+          <iframe className={css.video} width="560" height="315" src="https://www.youtube.com/embed/OoE-yuzmoHQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
       </div>
     </div>
