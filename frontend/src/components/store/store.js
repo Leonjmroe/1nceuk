756ea9e44f68b4e9f9562f66e124fde7fa0ce4a8
Leonjmroe <leonjmroe@gmail.com>
2023-06-11 17:29:30 +0100

    skate page update UI

--- frontend/src/components/store/store.js
+++ frontend/src/components/store/store.js
@@ -81,35 +81,35 @@ export function StoreSelect() {
     <div className={css.store_select_container}>
        <div className={css.item_select_container}>
          <div className={css.sqrText}>Hats</div>
-         <div className={`${css.sqrImage} ${css.img4}`} onClick={()=> navigate('/store', {state: {catagory: 'hats'} })} />
+         <div className={`${css.sqrImage} ${css.hats}`} onClick={()=> navigate('/store', {state: {catagory: 'hats'} })} />
        </div>
        <div className={css.item_select_container}>
          <div className={css.sqrText}>Hoodies</div>
-         <div className={`${css.sqrImage} ${css.img1}`} onClick={()=> navigate('/store', {state: {catagory: 'hoodies'} })} />
+         <div className={`${css.sqrImage} ${css.hoodies}`} onClick={()=> navigate('/store', {state: {catagory: 'hoodies'} })} />
        </div>
        <div className={css.item_select_container}>
          <div className={css.sqrText}>Jackets</div>
-         <div className={`${css.sqrImage} ${css.img2}`} onClick={()=> navigate('/store', {state: {catagory: 'jackets'} })} />
+         <div className={`${css.sqrImage} ${css.jackets}`} onClick={()=> navigate('/store', {state: {catagory: 'jackets'} })} />
        </div>
        <div className={css.item_select_container}>
          <div className={css.sqrText}>Joggers</div>
-         <div className={`${css.sqrImage} ${css.img3}`} onClick={()=> navigate('/store', {state: {catagory: 'joggers'} })} />
+         <div className={`${css.sqrImage} ${css.joggers}`} onClick={()=> navigate('/store', {state: {catagory: 'joggers'} })} />
        </div>
        <div className={css.item_select_container}>
          <div className={css.sqrText}>Shorts</div>
-         <div className={`${css.sqrImage} ${css.img3}`} onClick={()=> navigate('/store', {state: {catagory: 'shorts'} })} />
+         <div className={`${css.sqrImage} ${css.shorts}`} onClick={()=> navigate('/store', {state: {catagory: 'shorts'} })} />
        </div>
        <div className={css.item_select_container}>
          <div className={css.sqrText}>T-Shirts</div>
-         <div className={`${css.sqrImage} ${css.img2}`} onClick={()=> navigate('/store', {state: {catagory: 't-shirts'} })} />
+         <div className={`${css.sqrImage} ${css.tshirts}`} onClick={()=> navigate('/store', {state: {catagory: 't-shirts'} })} />
        </div>
        <div className={css.item_select_container}>
          <div className={css.sqrText}>Jumpers</div>
-         <div className={`${css.sqrImage} ${css.img1}`} onClick={()=> navigate('/store', {state: {catagory: 'jumpers'} })} />
+         <div className={`${css.sqrImage} ${css.jumpers}`} onClick={()=> navigate('/store', {state: {catagory: 'jumpers'} })} />
        </div>
        <div className={css.item_select_container}>
          <div className={css.sqrText}>Accessories</div>
-         <div className={`${css.sqrImage} ${css.img4}`} onClick={()=> navigate('/store', {state: {catagory: 'accessories'} })} />
+         <div className={`${css.sqrImage} ${css.accessories}`} onClick={()=> navigate('/store', {state: {catagory: 'accessories'} })} />
        </div>
     </div>
   );
