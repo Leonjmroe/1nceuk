77c88f0567b2970baa21079929a564bb11e13d44
Leonjmroe <leonjmroe@gmail.com>
2023-06-11 15:28:09 +0100

    server fix

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -16,7 +16,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 X_FRAME_OPTIONS = 'ALLOW-FROM https://www.youtube.com'
 
-Production code
+# Production code
 SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
