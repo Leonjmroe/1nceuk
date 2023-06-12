f605eb0f171ff1bf2a571da5b42334ba53113901
Leonjmroe <leonjmroe@gmail.com>
2023-06-12 22:14:49 +0100

    skate page modify, contact page mobile

--- frontend/src/components/skating/skating.js
+++ frontend/src/components/skating/skating.js
@@ -8,14 +8,14 @@ export default function Skating() {
         <div className={css.skating_title}>Skating</div>
         <div className={css.content_cont}>
           <div className={css.main_text}>
-              Within this community, it is customary to acknowledge skateboarders by admiring their attire. Our ultimate objective was to establish a company that not only caters to the clothing needs of skaters but also understands their subculture. By supplying the local skating community with a distinct identity, we aimed to facilitate recognition and connection among skaters of all levels. Through the medium of clothing, 1nceuk endeavors to unify us all, transcending individual skill levels.
+            Swansea has had massive roots in skateboarding since the 70s and the community still remains strong, Within this community theres abundance of talented individuals in skate and art alike.  it is customary to acknowledge skateboarders as those who admire their attire. By supplying the local skating community with a distinct identity, we aimed to facilitate recognition and connection among skaters of all levels.Through the medium of clothing, 1NCEUK endeavors to unify the creative and ambitious minds, from filmmakers to skaters and artists within our community, creating fun skate projects such as films as a result.     
           </div>
           <div className={css.main_image}></div>
-          <div className={css.text_1}>Text about skate vid 1</div>
+          {/*<div className={css.text_1}>Text about skate vid 1</div>*/}
           <iframe className={css.video_1} width="560" height="315" src="https://www.youtube.com/embed/xM8uQir3SOw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
           <iframe className={css.video_2} width="560" height="315" src="https://www.youtube.com/embed/OoE-yuzmoHQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-          <div className={css.text_2}>Text about skate vid 2</div>
-          <div className={css.text_3}>Text about skate vid 3</div>
+{/*          <div className={css.text_2}>Text about skate vid 2</div>
+          <div className={css.text_3}>Text about skate vid 3</div>*/}
           <iframe className={css.video_3} width="560" height="315" src="https://www.youtube.com/embed/OoE-yuzmoHQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
       </div>
