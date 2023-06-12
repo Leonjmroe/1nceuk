ee7752f043e986ef1c6f4d10c7480ba0bf35e858
Leonjmroe <leonjmroe@gmail.com>
2023-06-12 17:06:04 +0100

    skate page mobile

--- frontend/src/components/skating/skating.js
+++ frontend/src/components/skating/skating.js
@@ -7,16 +7,16 @@ export default function Skating() {
       <div className={css.skating_cont}>
         <div className={css.skating_title}>Skating</div>
         <div className={css.content_cont}>
-          <div className={`${css.main_text} ${css.text}`}>
+          <div className={css.main_text}>
               Within this community, it is customary to acknowledge skateboarders by admiring their attire. Our ultimate objective was to establish a company that not only caters to the clothing needs of skaters but also understands their subculture. By supplying the local skating community with a distinct identity, we aimed to facilitate recognition and connection among skaters of all levels. Through the medium of clothing, 1nceuk endeavors to unify us all, transcending individual skill levels.
           </div>
           <div className={css.main_image}></div>
-          <div className={css.text}>Text about skate vid 1</div>
-          <iframe className={css.video} width="560" height="315" src="https://www.youtube.com/embed/xM8uQir3SOw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-          <iframe className={css.video} width="560" height="315" src="https://www.youtube.com/embed/OoE-yuzmoHQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-          <div className={css.text}>Text about skate vid 2</div>
-          <div className={css.text}>Text about skate vid 3</div>
-          <iframe className={css.video} width="560" height="315" src="https://www.youtube.com/embed/OoE-yuzmoHQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <div className={css.text_1}>Text about skate vid 1</div>
+          <iframe className={css.video_1} width="560" height="315" src="https://www.youtube.com/embed/xM8uQir3SOw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <iframe className={css.video_2} width="560" height="315" src="https://www.youtube.com/embed/OoE-yuzmoHQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+          <div className={css.text_2}>Text about skate vid 2</div>
+          <div className={css.text_3}>Text about skate vid 3</div>
+          <iframe className={css.video_3} width="560" height="315" src="https://www.youtube.com/embed/OoE-yuzmoHQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
       </div>
     </div>
