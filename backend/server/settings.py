3c681de7c3a346f9bd98f70f41d8b2143746da97
Leonjmroe <leonjmroe@gmail.com>
2023-06-13 22:00:07 +0100

    CSP fix, AWS image test

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -137,7 +137,7 @@ if IS_PROD:
 
 # Example CSP header configuration in Django settings.py
 CSP_HEADER = {
-    'default-src': "'self'",
+    'default-src': "'self' 'unsafe-inline'",
     'script-src': "'self' 'unsafe-inline' 'unsafe-eval'",
     'style-src': "'self' 'unsafe-inline'",
     # Other directives...
