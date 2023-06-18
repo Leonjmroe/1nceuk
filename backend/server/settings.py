e6b8109d91e4c50b17f4553b213df9b302217d07
Leonjmroe <leonjmroe@gmail.com>
2023-06-18 19:24:47 +0100

    productionise

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -17,17 +17,17 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 X_FRAME_OPTIONS = 'ALLOW-FROM https://www.youtube.com'
 
-# # # Production code -------
-# SECURE_SSL_REDIRECT = True
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# # Production code -------
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# # Check if the app is running in production
-# IS_PROD = os.environ.get('IS_PROD', False)
+# Check if the app is running in production
+IS_PROD = os.environ.get('IS_PROD', False)
 
-# # If the app is running in production, enable HTTPS redirect
-# if IS_PROD:
-#     SECURE_SSL_REDIRECT = True
-#     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# If the app is running in production, enable HTTPS redirect
+if IS_PROD:
+    SECURE_SSL_REDIRECT = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
 
