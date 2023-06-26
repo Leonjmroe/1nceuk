dcb790041da7353f42ccfddc3f29323eaf286150
Leonjmroe <leonjmroe@gmail.com>
2023-06-26 20:00:25 +0100

    cloudfront rollback

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -184,12 +184,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # ---- AWS -----
 
-AWS_S3_CUSTOM_DOMAIN = os.getenv("CLOUDFRONT_DOMAIN")
+# AWS_S3_CUSTOM_DOMAIN = os.getenv("CLOUDFRONT_DOMAIN")
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-# AWS_S3_CUSTOM_DOMAIN = os.environ.get('AWS_S3_CUSTOM_DOMAIN')                                 
+AWS_S3_CUSTOM_DOMAIN = os.environ.get('AWS_S3_CUSTOM_DOMAIN')                                 
 AWS_DEFAULT_ACL = os.environ.get('AWS_DEFAULT_ACL')     
 
 MEDIA_URL = '/mediafiles/'
